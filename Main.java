245f2fba45b9a52e116615b9d72c03733269048e
Daniel Dahal <1991baburamdahal@gmail.com>
2018-03-18 19:37:27 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package boxproject;
+/**
+ * Main.java
+ */
+public class Main {
+ 
+        public static void main(String[] args) {
+ 
+           MatchBox mb1 = new MatchBox(5, 10, 3);
+           mb1.getVolume();
+           mb1.getWeight();
+        }
+}
